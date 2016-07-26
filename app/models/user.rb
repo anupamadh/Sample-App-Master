610e8004615d326a59728fb8a07b0e0df080ac23
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-27 04:35:05 +0800

    added password-digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-
-  # VALIDATION
-
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :name,
-            presence: true,
-            length: { maximum: 5, message: 'Too long yow' }
-
-  validates :email,
-            presence: true,
-            length: { maximum: 50 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-            
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
