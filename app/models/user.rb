1ec44d8567756656cdf9a4f76bfa83aaebee9be8
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-27 03:21:54 +0800

    Sample App latest

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+
+  # VALIDATION
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,
+            presence: true,
+            length: { maximum: 5, message: 'Too long yow' }
+
+  validates :email,
+            presence: true,
+            length: { maximum: 50 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+            
+  validates :password, presence: true, length: { minimum: 6 }
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  resources :posts
+  resources :users
+# resources :posts
+root 'static_pages#home' # controller#method
+#
+get '/home', to: 'static_pages#home'
+get '/about', to: 'static_pages#about'
+get '/help', to: 'static_pages#help'
+get '/contact', to: 'static_pages#contact'
+ get  '/signup',  to: 'users#new'
+  
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
